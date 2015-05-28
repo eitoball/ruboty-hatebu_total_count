ba71e38e74105a620edfa880ab1608d1690f0600
mzp <mzpppp@gmail.com>
2015-05-28 14:32:46 +0900

    fix regexp group name

--- lib/ruboty/handlers/hatebu_total_count.rb
+++ lib/ruboty/handlers/hatebu_total_count.rb
@@ -1,7 +1,7 @@
 module Ruboty
   module Handlers
     class HatebuTotalCount < Base
-      on /count (?<url>\S+)/, description: 'count total bookmarks', name: :count
+      on /count (?<site>\S+)/, description: 'count total bookmarks', name: :count
 
       def count(message)
         Ruboty::HatebuTotalCount::Actions::Count.new(message).call
